f745effaf07eef476fbd9c42b92406afd3111765
mr-cade <cadelpetersen@gmail.com>
2020-10-20 23:39:28 -0600

    calendar formatting

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./style.css";
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 
 const localizer = momentLocalizer(moment)
 const myEventsList = []
