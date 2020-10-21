f806b5ee8e1f8c79f212348b5bb50bad909089c4
baduraje@isu.edu <baduraje@isu.edu>
2020-10-21 15:22:50 -0600

    ClASSES menu item now leads to caledar and Carousel is now being displayed in the homepage but it needs to be looked at

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import HeaderNav from "../components/Header/index"
+import Carousel from "../components/Carousel/index"
+
 
 export default function Header() {
     return (
         <div>
-           <HeaderNav>
-               
-           </HeaderNav>
+           <HeaderNav> </HeaderNav>
+           <Carousel></Carousel>
         </div>
     )
 }
