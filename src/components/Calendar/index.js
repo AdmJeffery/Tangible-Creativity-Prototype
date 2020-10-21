5c4331299bcaaca44edffe96b4465e015356705c
mr-cade <cadelpetersen@gmail.com>
2020-10-20 23:02:25 -0600

    calendar outline

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./style.css";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
-export default function Calendar() {
+const localizer = momentLocalizer(moment)
+const myEventsList = []
+
+export default function CalendarForm() {
     return (
         <div>
-            
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     )
 }
