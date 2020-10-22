5d20b3b09630c6509129d4384b3c6bb0b229902f
mr-cade <cadelpetersen@gmail.com>
2020-10-22 13:27:41 -0600

    calendar date input

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,24 +4,24 @@ import "./style.css";
 export default function HeaderNav() {
     return (
         <div>
-            <nav class="navbar   navbar-expand-lg navbar-blue bg-white">
-                <a class="navbar-brand " href="/home">Tangible Creativity</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar   navbar-expand-lg navbar-blue bg-white">
+                <a className="navbar-brand " href="/home">Tangible Creativity</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-                        <ul class="navbar-nav ">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="/home">HOME <span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                        <ul className="navbar-nav ">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/home">HOME <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href ="/calendar">CLASSES</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href ="/calendar">CLASSES</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/about">ABOUT</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">ABOUT</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/contact">CONTACT</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contact">CONTACT</a>
                             </li>
                         </ul>
                 </div>
