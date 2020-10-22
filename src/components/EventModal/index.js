3ba8f2d97339996c0f9c02accbaa65b87227232f
mr-cade <cadelpetersen@gmail.com>
2020-10-22 16:59:24 -0600

    modal taking shape

--- src/components/EventModal/index.js
+++ src/components/EventModal/index.js
@@ -1,58 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
- 
+
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
 };
- 
-export default function EventModal(){
-  var subtitle;
-  const [modalIsOpen,setIsOpen] = React.useState(false);
-  function openModal() {
-    setIsOpen(true);
-  }
- 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
- 
-  function closeModal(){
-    setIsOpen(false);
-  }
- 
+
+export default function EventModal() {
+    var subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    function saveEvent() {
+        //   save event to calendar
+    }
+
     return (
-      <div>
-        <button onClick={openModal}>Open Modal</button>
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
- 
-          <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
-      </div>
+        <div>
+            <button onClick={openModal}>Add Event</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Event Modal"
+            >
+
+                <h2 ref={_subtitle => (subtitle = _subtitle)}>Add an Event</h2>
+                <button onClick={closeModal}>close</button>
+                <button onClick={saveEvent}>save</button>
+                <div class="form-group">
+                    <label for="">Event Title</label>
+                    <input type="text"
+                        class="form-control" name="" id="" aria-describedby="helpId" placeholder=""></input>
+                    <label for="">Date</label>
+                    <input type="date"
+                        class="form-control" name="" id="" aria-describedby="helpId" placeholder=""></input>
+                </div>
+            </Modal>
+        </div>
     );
 }
- 
+
 
