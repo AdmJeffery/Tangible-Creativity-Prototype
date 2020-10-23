24b1fd090345532289a7701b64d74c881def5720
Ammon Jeffery <savingphace@gmail.com>
2020-10-22 21:11:06 -0600

    added skeletal context into footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,12 +8,23 @@ export default function Footer() {
         <footer>
             <Row>
                 <Col size = "md-6">
-                <h2>Tangible Creativity</h2>
+                <h5>Tangible Creativity</h5>
                 <br/>
                 <p>Unleash your creative side through hands-on instruction.</p>
                 </Col>
                 <Col size = "md-6">
-                
+                <h5>Contact Hours</h5>
+                <p>Mon:</p>
+                <p>Tues:</p>
+                <p>Wed:</p>
+                <p>Thurs:</p>
+                <p>Fri:</p>
+                <p>Sat:</p>
+                <p>Sun:</p>
+                <br/>
+                <p>Located at:</p>
+                <br/>
+                <p>Phone: </p>
                 </Col>
             </Row>
         </footer>
