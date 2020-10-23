d7dac32435757a0a2793a485787bef46d21878ab
baduraje@isu.edu <baduraje@isu.edu>
2020-10-23 01:46:55 -0600

    carousel slide show now working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Carousel from "../components/Carousel/index"
+import Example from "../components/Carousel/index"
 import Footer from "../components/Footer/index"
 
 export default function Header() {
     return (
         <div>
-           <Carousel></Carousel>
-            <Footer></Footer>
+           <Example/>
+           
         </div>
     )
 }
