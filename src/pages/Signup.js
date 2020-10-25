5e3d3c87517f1d65e261b2873cc1c8733eed1d8b
mr-cade <cadelpetersen@gmail.com>
2020-10-25 15:57:15 -0600

    newsletter signup form added. now needs post route to submit name and email

---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewsletterSignup from "../components/NewsletterSignup/index"
+
+export default function Signup() {
+    return (
+        <div class="calendar">
+            <NewsletterSignup/>
+            {/* <br></br>
+            <EventModal /> */}
+        </div>
+    )
+}
