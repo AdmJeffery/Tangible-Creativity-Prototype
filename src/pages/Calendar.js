e7e0984b3ad92336aead2e091b6816ed1ea8ac5f
mr-cade <cadelpetersen@gmail.com>
2020-10-26 13:33:55 -0600

    event modal removed

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import CalendarForm from "../components/Calendar/index"
-import EventModal from '../components/EventModal'
 
 export default function Calendar() {
     return (
         <div class="calendar">
             <CalendarForm>
             </CalendarForm>
-            {/* <br></br>
-            <EventModal /> */}
+           
         </div>
     )
 }
