73003151fc79f64de343dec792ba01a557e6350f
baduraje@isu.edu <baduraje@isu.edu>
2020-10-27 20:34:35 -0600

    made changes to handlechange functions in Newsletter page

--- src/components/NewsletterSignup/index.js
+++ src/components/NewsletterSignup/index.js
@@ -7,16 +7,11 @@ class NewsletterSignup extends React.Component {
         super(props);
         this.state = { name: '', email: '' };
 
-        this.handleChange = this.handleChange.bind(this);
+       this.handleNameChange = this.handleNameChange.bind(this);
+       this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({
-            name: event.target.value,
-            email: event.target.value
-        });
-    }
 
     handleSubmit(event) {
         alert('A name was submitted: ' + this.state.name);
@@ -26,7 +21,25 @@ class NewsletterSignup extends React.Component {
         //   need to write a post route here 
     }
 
+    handleNameChange(event) {
+        this.setState({
+            name: event.target.value
+           
+           
+        });   
+    }
+    
+    handleEmailChange(event) {
+        this.setState({
+           
+            email: event.target.value
+           
+        });   
+    }
+    
+
     render() {
+    
         return (
             <div>
                 
@@ -35,7 +48,7 @@ class NewsletterSignup extends React.Component {
                         <div className="card-title"><h4>Our Newsletter</h4></div>
                         <div className="card-text">Stay up-to-date with all things Tangible Creativity! New crafts, events, and news will be at your fingertips so you never miss a beat! Craft on!</div>
                     </div>
-                    <img src={"../../../public/pictures/Wire-Tree.jpg"} className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""></img>
+                    <img src="" className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""></img>
                 </div>
                 
                 <div className="card col-md-4 m-5" id="signupCard">
@@ -45,15 +58,15 @@ class NewsletterSignup extends React.Component {
                         <label>
                             Name:
                     <br></br>
-                            <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
+                            <input type="text" name="name" value={this.state.value} onChange={this.handleNameChange} />
                         </label>
                         <label>
                             Email:
                     <br></br>
-                            <input type="email" name="email" value={this.state.value} onChange={this.handleChange} />
+                            <input type="email" name="email" value={this.state.value} onChange={this.handleEmailChange} />
                         </label>
                         <br></br>
-                        <input type="submit" className="btn-primary" value="Subscribe!" />
+                        <input type="submit" className="btn-primary" value="Subscribe!"/>
                     </form>
                 </div>
             </div>
